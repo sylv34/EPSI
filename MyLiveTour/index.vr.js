0d26e5dea814707140f539293c287fed7464f6e8
unknown <sylvain.dienst@gmail.com>
2018-10-02 16:26:42 +0200

    J2

--- MyLiveTour/index.vr.js
+++ MyLiveTour/index.vr.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { AppRegistry } from 'react-vr';
 import { LiveTour } from 'live-tour-lab';
-import Hero from './Hero';
 
 export default class MyLiveTour extends React.Component {
   render() {
     return (
-      <LiveTour tourURI='hello-world.json'/>
+      <LiveTour tourURI='epsi.json'/>
     );
   }
 };
